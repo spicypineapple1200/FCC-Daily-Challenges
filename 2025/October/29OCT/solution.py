00f843a09c9c66cc3885cf905fd53392744d51de
Brendan Grigsby <141603988+spicypineapple1200@users.noreply.github.com>
2025-10-30 13:01:59 -0500

    Update solution.py

--- 2025/October/29OCT/solution.py
+++ 2025/October/29OCT/solution.py
@@ -1,25 +1,22 @@
-
 def sort(emails):
-    emails = sorted(emails)
-    
     w_domain_order = sorted(set([item.split('@')[1].lower() for item in emails]))
-    domain_order = sorted(set([item.split('@')[1].lower() for item in emails]))
-    print(w_domain_order)
-    
-    name_order = sorted(set([item.split('@')[0] for item in emails]))
-    print(domain_order)
-    print(name_order)
-    
     answer = []
-    
-    
-
-    # answer = sorted(answer)
-    # print(emails)
+    for w_domain in w_domain_order:
+        name_list = []
+        for email in emails:
+            domain = email.split('@')[1]
+            if domain.lower() == w_domain:
+                name = email.split('@')[0]
+                name_list.append(name)
+            name_list = sorted(name_list, key=str.lower)
+        answer+=name_list
+        name_list = answer
+        answer = []
+        for w_domain in w_domain_order:
+            for name in name_list:
+                for email in emails:
+                    domain = email.split('@')[1].lower()
+                    if name in email and w_domain == domain and email not in answer:
+                        answer.append(email)
     print(answer)
-
-# sort(["jill@mail.com", "john@example.com", "jane@example.com"])
-# should return ["jane@example.com", "john@example.com", "jill@mail.com"].
-
-sort(["sam@MAIL.com", "amy@mail.COM", "bob@Mail.com"])
-# should return ["amy@mail.COM", "bob@Mail.com", "sam@MAIL.com"].
+    return answer
