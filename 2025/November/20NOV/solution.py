f67dcb546443416489e79c83db52f2982a7673f2
Brendan Grigsby <141603988+spicypineapple1200@users.noreply.github.com>
2025-11-20 13:07:59 -0600

    Update solution.py

--- 2025/November/20NOV/solution.py
+++ 2025/November/20NOV/solution.py
@@ -1,6 +1,6 @@
 def longest_word(sentence):
-    no = ',.!\'?'
-    sentence = list((''.join([i for i in sentence if i not in no])).split(' '))
+    extras = ',.!\'?'
+    sentence = (''.join([bit for bit in sentence if bit not in extras])).split(' ')
     check = [len(word) for word in sentence]
     answer = sentence[check.index(max(check))]
     print(answer)
