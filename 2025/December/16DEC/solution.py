9699f0a8c5e57dc0e092bbbab3fdd06c940237d3
Brendan Grigsby <141603988+spicypineapple1200@users.noreply.github.com>
2025-12-16 10:48:20 -0600

    Create solution.py

---
+++ 2025/December/16DEC/solution.py
@@ -0,0 +1,11 @@
+import string
+
+def has_consonant_count(text, target):
+    stri = string.ascii_letters
+    cons, num = ("a", "e", "i", "o", "u"), 0
+    for letter in text:
+        if letter not in cons and letter in stri: num+=1
+    if num == target: answer = True
+    else: answer = False
+    print(answer)
+    return answer
